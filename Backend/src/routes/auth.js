3e33b2569ee5133a9c4a02071779b9ffb1087c98
Grabkaazz <danielarnold2003@gmail.com>
2023-11-06 15:41:00 +0100

    added calculation and login api

--- Backend/src/routes/auth.js
+++ Backend/src/routes/auth.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { loginByUsernamePassword } = require('../controllers/auth');
+const { Pool } = require('pg');
+
+// Create a new connection pool
+const pool = new Pool({
+  user: 'your_username',
+  host: 'your_host',
+  database: 'your_database',
+  password: 'your_password',
+  port: 5432, // or your PostgreSQL port
+});
 
 // login user
 router.post('/login', async (req, res, next) => {
@@ -8,12 +17,22 @@ router.post('/login', async (req, res, next) => {
     const { username = '', password = '' } = req.body;
     const { expiresInMins = 60 } = req.body;
 
-    const payload = await loginByUsernamePassword({
-      username,
-      password,
-      expiresInMins,
-    });
+    // Query the database for the user's login information
+    const { rows } = await pool.query('SELECT * FROM users WHERE username = $1 AND password = $2', [username, password]);
 
+    // If the user is not found, return an error
+    if (rows.length === 0) {
+      return res.status(401).send('Invalid username or password');
+    }
+
+    // Otherwise, create a payload with the user's information
+    const payload = {
+      id: rows[0].id,
+      username: rows[0].username,
+      expiresIn: new Date(Date.now() + expiresInMins * 60000),
+    };
+
+    // Return the payload
     res.send(payload);
   } catch (error) {
     next(error);
@@ -21,3 +40,4 @@ router.post('/login', async (req, res, next) => {
 });
 
 module.exports = router;
+
--- Backend/src/routes/avg.js
+++ Backend/src/routes/avg.js
@@ -1,11 +1,30 @@
-function calculateAverage(req, res) {
-    const numbers = req.body.numbers;
-    if (!numbers || !Array.isArray(numbers)) {
-        return res.status(400).json({ error: 'Invalid input' });
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: 'your_user',
+    host: 'your_host',
+    database: 'your_database',
+    password: 'your_password',
+    port: 5432,
+});
+
+async function calculateAverage(req, res) {
+    try {
+        const client = await pool.connect();
+        const result = await client.query('SELECT * FROM numbers');
+        const numbers = result.rows.map(row => row.number);
+        if (!numbers || !Array.isArray(numbers)) {
+            return res.status(400).json({ error: 'Invalid input' });
+        }
+        const sum = numbers.reduce((acc, curr) => acc + curr, 0);
+        const avg = sum / numbers.length;
+        return res.json({ average: avg });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Internal server error' });
+    } finally {
+        client.release();
     }
-    const sum = numbers.reduce((acc, curr) => acc + curr, 0);
-    const avg = sum / numbers.length;
-    return res.json({ average: avg });
 }
 
 module.exports = { calculateAverage };

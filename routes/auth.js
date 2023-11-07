1a08f984c9ae577356d84f06ce74b4dc527753e0
Grabkaazz <danielarnold2003@gmail.com>
2023-11-07 19:55:30 +0100

    database datatransfer

--- routes/auth.js
+++ routes/auth.js
@@ -10,7 +10,7 @@ const pool = new Pool({
   host: '    ec2-34-242-199-141.eu-west-1.compute.amazonaws.com',
   database: 'd967mmgnsklhd0',
   password: 'd7036d55422fa5330f1a78999dc85500b8e57b5611226416b9329639579fabe4',
-  port: 5432, // or your PostgreSQL port
+  port: 5432,
 });
 
 //Testing the connection
@@ -27,7 +27,7 @@ router.route('/')
       const { expiresInMins = 60 } = req.body;
 
       // Query the database for the user's login information
-      const { rows } = await pool.query('SELECT * FROM users WHERE email = $1 AND password = $2', [email, password]);
+      const { rows } = await pool.query('SELECT * FROM users WHERE email = $1 AND password = $3', [email, password]);
 
       // If the user is not found, return an error
       if (rows.length === 0) {

a9ec5df23996b0ecc1781f28af68f3ca3f1c15c4
Grabkaazz <danielarnold2003@gmail.com>
2023-11-07 11:55:33 +0100

    commit to pull correct version

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const routes = require('./routes');
+const routes = require('/routes');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -11,3 +11,4 @@ app.use('/api', routes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+

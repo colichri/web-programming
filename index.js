78a3cb5b9dafb49495d093327b9346d7762f61cb
Colin Christ <colin.christ@icloud.com>
2023-11-07 12:00:32 +0100

    add port 3000 to index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const port = 3000;
 
-const routes = require('/routes');
+const routes = require("/routes");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api', routes);
+app.use("/api", routes);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-

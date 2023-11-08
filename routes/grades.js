a2d8dd30d23a6f37bcc934ee6718342a80b8a9b3
Grabkaazz <danielarnold2003@gmail.com>
2023-11-08 08:32:48 +0100

    added host database in grades

--- routes/grades.js
+++ routes/grades.js
@@ -7,10 +7,10 @@ const bodyParser = require('body-parser');
 
 // Create a new pool to connect to the database
 const pool = new Pool({
-    user: 'your_username',
-    host: 'your_host',
-    database: 'your_database',
-    password: 'your_password',
+    user: 'miacqskbeyafwb',
+    host: 'ec2-34-242-199-141.eu-west-1.compute.amazonaws.com',
+    database: 'd967mmgnsklhd0',
+    password: 'd7036d55422fa5330f1a78999dc85500b8e57b5611226416b9329639579fabe4',
     port: 5432,
 });
 
